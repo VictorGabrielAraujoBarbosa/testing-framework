c30b0ff713eb92a26038af8f1b60692236570ab7
Victor <140446172+VictorGabrielAraujoBarbosa@users.noreply.github.com>
2025-09-30 20:18:31 -0300

    7. Executando Todos os Testes
    Esse commit atualiza o OverallTest.py para executar todos os testes

--- OverallTest.py
+++ OverallTest.py
@@ -1,11 +1,19 @@
 from TestLoader import TestLoader
-from TestLoaderTest import TestLoaderTest
+from TestSuite import TestSuite
 from TestRunner import TestRunner
-from TestResult import TestResult
+from TestLoaderTest import TestLoaderTest
+from TestSuiteTest import TestSuiteTest
+from TestCaseTest import TestCaseTest
 
-result = TestResult()
 loader = TestLoader()
-suite = loader.make_suite(TestLoaderTest)
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_load_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_load_suite)
 
 runner = TestRunner()
 runner.run(suite)
